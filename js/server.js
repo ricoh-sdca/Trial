bb0816495f5b2db0cc9b97eb68927e913b7e54ec
Zhenyu <zhenyu.lu@ricoh-usa.com>
2020-01-23 16:50:35 -0800

    UI change

---
+++ js/server.js
@@ -0,0 +1,15 @@
+var serverHost = "devu";
+
+/*
+function getEnvironment(hostname) {
+	alert(hostname);
+    if (hostname.match("^localhost")) {​
+        return "local";​
+    } else {​
+        return hostname.match(/^help-us\.(\S+)\.smart-integration.ricoh.com/)[1];​
+    }​
+}
+serverHost = getEnvironment(window.location.host);
+*/
+
+//alert(serverHost);
